f02549300c421719496f38c9ecfb09ef4aa271a9
peter88213 <peter.88213@t-online.de>
2021-07-13 18:41:48 +0200

    Update csv converter

---
+++ src/build_cnv_timeline.py
@@ -0,0 +1,38 @@
+""" Build a python script for the cnv_timeline distribution.
+        
+In order to distribute a single script without dependencies, 
+this script "inlines" all modules imported from the pywriter package.
+
+The PyWriter project (see see https://github.com/peter88213/PyWriter)
+must be located on the same directory level as the yw2md project. 
+
+For further information see https://github.com/peter88213/paeon
+Published under the MIT License (https://opensource.org/licenses/mit-license.php)
+"""
+import os
+import inliner
+
+SRC = '../src/'
+BUILD = '../test/'
+SOURCE_FILE = 'cnv_timeline.py'
+TARGET_FILE = BUILD + 'cnv_timeline.py'
+
+
+def main():
+    os.chdir(SRC)
+
+    try:
+        os.remove(TARGET_FILE)
+
+    except:
+        pass
+
+    inliner.run(SOURCE_FILE,
+                TARGET_FILE, 'paeon', '../src/')
+    inliner.run(TARGET_FILE,
+                TARGET_FILE, 'pywriter', '../../PyWriter/src/')
+    print('Done.')
+
+
+if __name__ == '__main__':
+    main()
--- src/cnv_timeline.py
+++ src/cnv_timeline.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Create a yWriter project from an Aeon 2 csv timeline. 
+"""yWriter/csv timeline converter. 
 
 Version @release
 
@@ -7,22 +7,53 @@ Copyright (c) 2021 Peter Triesberger
 For further information see https://github.com/peter88213/Paeon
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
-SUFFIX = ''
+import os
+import argparse
 
-import sys
-
-from pywriter.ui.ui_tk import UiTk
+from pywriter.ui.ui import Ui
+from pywriter.ui.ui_cmd import UiCmd
 from paeon.csv.csv_converter import CsvConverter
 
+SUFFIX = ''
 
-def run(sourcePath, suffix=''):
-    ui = UiTk('yWriter import/export')
+
+def run(sourcePath, silentMode=True):
+
+    if silentMode:
+        ui = Ui('')
+
+    else:
+        ui = UiCmd('yWriter/csv timeline converter @release')
+
     converter = CsvConverter()
     converter.ui = ui
-    kwargs = {'suffix': suffix}
+    kwargs = {'suffix': SUFFIX}
     converter.run(sourcePath, **kwargs)
     ui.start()
 
 
 if __name__ == '__main__':
-    run(sys.argv[1], SUFFIX)
+    parser = argparse.ArgumentParser(
+        description='yWriter/csv timeline converter',
+        epilog='')
+    parser.add_argument('sourcePath', metavar='Sourcefile',
+                        help='The path of the tineline csv file or yWriter project file.')
+
+    parser.add_argument('--silent',
+                        action="store_true",
+                        help='suppress error messages and the request to confirm overwriting')
+    args = parser.parse_args()
+
+    if args.silent:
+        silentMode = True
+
+    else:
+        silentMode = False
+
+    if os.path.isfile(args.sourcePath):
+        sourcePath = args.sourcePath
+
+    else:
+        sourcePath = None
+
+    run(sourcePath, silentMode)
