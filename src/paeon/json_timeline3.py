e5254632a83629241267a47892ac87e39c05187a
peter88213 <peter.88213@t-online.de>
2021-10-31 18:15:27 +0100

    Fix docstrings

--- src/paeon/json_timeline3.py
+++ src/paeon/json_timeline3.py
@@ -1,7 +1,7 @@
 """Provide a class for Aeon Timeline 3 JSON representation.
 
 Copyright (c) 2021 Peter Triesberger
-For further information see https://github.com/peter88213/aeon3yw
+For further information see https://github.com/peter88213/paeon
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 import os
