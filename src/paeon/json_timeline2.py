42b8fabe3ad1e90afd80a706995b1f2468d2dfef
peter88213 <peter.88213@t-online.de>
2021-11-13 13:45:15 +0100

    Add merge and write methods to JsonTimeline2

--- src/paeon/json_timeline2.py
+++ src/paeon/json_timeline2.py
@@ -391,3 +391,92 @@ class JsonTimeline2(Novel):
                     self.chapters[chIdNarrative].srtScenes.append(scId)
 
         return 'SUCCESS: Data read from "' + os.path.normpath(self.filePath) + '".'
+
+    def merge(self, source):
+        """Update date/time/duration from the source,
+        if the scene title matches.
+        """
+        message = self.read()
+
+        if message.startswith('ERROR'):
+            return message
+
+        # Get scene titles.
+
+        scIdsByTitles = {}
+        scIdMax = 0
+
+        for scId in self.scenes:
+
+            if self.scenes[scId].title in scIdsByTitles:
+                return 'ERROR: Cannot update because of ambiguous scene titles.'
+
+            else:
+                scIdsByTitles[self.scenes[scId].title] = scId
+
+                if int(scId) > scIdMax:
+                    scIdMax = int(scId)
+
+        # Get date/time/duration from the source, if the scene title matches.
+
+        for srcId in source.scenes:
+
+            if source.scenes[srcId].title in scIdsByTitles:
+                scId = scIdsByTitles[source.scenes[srcId].title]
+
+                if source.scenes[srcId].date or source.scenes[srcId].time:
+
+                    if source.scenes[srcId].date is not None:
+                        self.scenes[scId].date = source.scenes[srcId].date
+
+                    if source.scenes[srcId].time is not None:
+                        self.scenes[scId].time = source.scenes[srcId].time
+
+                elif source.scenes[srcId].minute or source.scenes[srcId].hour or source.scenes[srcId].day:
+                    self.scenes[scId].date = None
+                    self.scenes[scId].time = None
+
+                if source.scenes[srcId].minute is not None:
+                    self.scenes[scId].minute = source.scenes[srcId].minute
+
+                if source.scenes[srcId].hour is not None:
+                    self.scenes[scId].hour = source.scenes[srcId].hour
+
+                if source.scenes[srcId].day is not None:
+                    self.scenes[scId].day = source.scenes[srcId].day
+
+                if source.scenes[srcId].lastsMinutes is not None:
+                    self.scenes[scId].lastsMinutes = source.scenes[srcId].lastsMinutes
+
+                if source.scenes[srcId].lastsHours is not None:
+                    self.scenes[scId].lastsHours = source.scenes[srcId].lastsHours
+
+                if source.scenes[srcId].lastsDays is not None:
+                    self.scenes[scId].lastsDays = source.scenes[srcId].lastsDays
+
+            else:
+                # Create a new event.
+
+                scIdMax += 1
+                newId = str(scIdMax)
+                self.scenes[newId] = source.scenes[srcId]
+                self.srtScenes.append(newId)
+
+        return 'SUCCESS'
+
+    def write(self):
+        """Write selected properties to the file.
+        """
+        jsonPart = extract_timeline(self.filePath)
+
+        if not jsonPart:
+            return 'ERROR: No JSON part found.'
+
+        elif jsonPart.startswith('ERROR'):
+            return jsonPart
+
+        try:
+            jsonData = json.loads(jsonPart)
+
+        except('JSONDecodeError'):
+            return 'ERROR: Invalid JSON data.'
